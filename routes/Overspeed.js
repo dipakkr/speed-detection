861270c4a278e0afe5562c8fa5aa3fd3e0d1d178
Deepak <raj.deeepak@gmail.com>
2018-03-22 02:43:38 +0530

    updated body

--- routes/Overspeed.js
+++ routes/Overspeed.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb');
+var timestamp = require('time-stamp');
+var request = require('request');
 
 router.get('/test',(req,res,next) => {
     res.send('API WORKING - OVERSPEEDING ROUTE');
@@ -13,21 +15,28 @@ router.post('/:car_number',(req,res,next) => {
 
     //DB CONNECTION
         MongoClient.connect('mongodb://127.0.0.1:27017/SpeedAnalyser',(err,client) =>{
-        if(err){
-            console.log('Unable to connect to database');
-        }
-        console.log('Succces DB connection');
-    
+
+        if(err) console.log('Unable to connect to database');
+
         const db = client.db('SpeedAnalyser');
-        db.collection(req.body.car_number).insertOne({
+
+        //Update speed_limit using the google road api, also update the overspeed
+
+        db.collection(req.params.car_number).insertOne({
+            location : "Delhi, India",
+            latitude : "28.7040592",
+            longitude : "77.10249019999992",
+            timestamp : timestamp('YYYY/MM/DD:mm:ss'),
             car_number : req.body.car_number,
-            speed : req.body.speed
+            speed : req.body.speed,
+            speed_limit : "65" + "KMPH",
+            overspeed : req.body.speed - 65
         })
+        
         .then(function(result){
             res.json({"code": 200 ,"message": "Data uploaded successfully"});
         });
-});
-
+    });
 });
 
 module.exports = router;
